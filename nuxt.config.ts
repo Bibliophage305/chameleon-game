1801608dd62cacfbb4869ec0f5616704f98fd673
Chris <christopher.hampson58@gmail.com>
2024-06-28 16:55:02 +0100

    Update prisma schema for prod db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@prisma/nuxt"],
+  prisma: {
+    generateClient: false,
+  },
   runtimeConfig: {
     public: {
       origin: process.env.ORIGIN,
